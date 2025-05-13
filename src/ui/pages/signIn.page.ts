a2560c15034dcf78dc52939da30348cc071e28c8
julyvilh <106986983+July-vilh@users.noreply.github.com>
2025-05-13 21:16:10 +0300

    fix signin

--- src/ui/pages/signIn.page.ts
+++ src/ui/pages/signIn.page.ts
@@ -4,15 +4,15 @@ import { IUser } from "types/user.types";
 export class SignIn extends SalesPortalPage {
   emailInput = this.page.locator("#emailinput");
   passwordInput = this.page.locator("#passwordinput");
- loginButton = this.page.getByRole("button", { name: "Login" });
+  loginButton = this.page.getByRole("button", { name: "Login" });
 
-  uniqueElement = this.buttonLogin;
+  uniqueElement = this.loginButton;
 
   async fillCredentials(userCreds: IUser) {
     await this.emailInput.fill(userCreds.email);
     await this.passwordInput.fill(userCreds.password);
   }
   async clickOnLoginButton() {
-    await this.buttonLogin.click();
+    await this.loginButton.click();
   }
 }
