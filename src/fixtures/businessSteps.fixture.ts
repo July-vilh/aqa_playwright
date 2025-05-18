3cce03dd58c048a1d63cf2e1c55717c8c6269139
julyvilh <106986983+July-vilh@users.noreply.github.com>
2025-05-18 15:07:36 +0300

    update hw-22

--- src/fixtures/businessSteps.fixture.ts
+++ src/fixtures/businessSteps.fixture.ts
@@ -1,9 +1,7 @@
+import { SignIn } from "ui/pages/signIn.page";
 import { test as base } from "./pages.fixture";
-import {
-  SALES_PORTAL_URL,
-  USER_LOGIN,
-  USER_PASSWORD,
-} from "config/environment";
+import { SALES_PORTAL_URL, USER_LOGIN, USER_PASSWORD } from "config/environment";
+import { IUser } from "types/user.types";
 
 interface IBusinessSteps {
   loginAsLocalUser: () => Promise<void>;
@@ -11,11 +9,16 @@ interface IBusinessSteps {
 
 export const test = base.extend<IBusinessSteps>({
   loginAsLocalUser: async ({ page, homePage }, use) => {
+    const signInPage = new SignIn(page); //создание объекта страницы
+    const userCreds: IUser = {
+      email: USER_LOGIN,
+      password: USER_PASSWORD,
+    };
+
     await use(async () => {
       await page.goto(SALES_PORTAL_URL);
-      await page.locator("#emailinput").fill(USER_LOGIN);
-      await page.locator("#passwordinput").fill(USER_PASSWORD);
-      await page.getByRole("button", { name: "Login" }).click();
+      await signInPage.fillCredentials(userCreds);
+      await signInPage.clickOnLoginButton();
       await homePage.waitForOpened();
     });
   },
